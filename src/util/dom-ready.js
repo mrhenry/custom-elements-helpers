0c032dbb9fea5dbc2649e2edb4f1a1bc0bfd5e85
Simon Menke <simon.menke@gmail.com>
2019-02-19 17:01:12 +0100

    Actually wait for dom ready

--- src/util/dom-ready.js
+++ src/util/dom-ready.js
@@ -1,16 +1,22 @@
 export default function waitForDOMReady() {
-	return new Promise((resolve) => {
-		if (document.readyState === 'complete') {
-			resolve();
-		} else {
-			const handler = function () {
-				if (document.readyState === 'complete') {
-					document.removeEventListener('readystatechange', handler, false);
-					resolve();
-				}
-			};
+  return new Promise(resolve => {
+    if (isInteractive()) {
+      resolve();
+    } else {
+      const handler = function() {
+        if (isInteractive()) {
+          document.removeEventListener("readystatechange", handler, false);
+          resolve();
+        }
+      };
 
-			document.addEventListener('readystatechange', handler, false);
-		}
-	});
+      document.addEventListener("readystatechange", handler, false);
+    }
+  });
+}
+
+function isInteractive() {
+  return (
+    document.readyState === "complete" || document.readyState === "interactive"
+  );
 }
